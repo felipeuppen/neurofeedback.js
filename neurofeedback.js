cb243103554931be283a726717ba01a3cc68ae18
Felipe <47905566+felipeuppen@users.noreply.github.com>
2024-06-18 11:50:29 -0400

    Update neurofeedback.js

--- neurofeedback.js
+++ neurofeedback.js
@@ -1,68 +1,5 @@
 // neurofeedback.js
 
-class FFT {
-    constructor(size, sampleRate) {
-        this.size = size;
-        this.sampleRate = sampleRate;
-        this.real = new Array(size).fill(0);
-        this.imag = new Array(size).fill(0);
-    }
-
-    forward(buffer) {
-        this.real = buffer.slice();
-        this.imag.fill(0);
-
-        const N = this.size;
-        const halfN = N / 2;
-        let real, imag, tpreal, tpimag, costp, sintp;
-        let cos = Math.cos, sin = Math.sin, PI = Math.PI;
-
-        for (let i = 0; i < N; i++) {
-            let j = 0;
-            for (let bit = 0; bit < Math.log2(N); bit++) {
-                j = (j << 1) | (1 & (i >> bit));
-            }
-            if (j > i) {
-                [this.real[i], this.real[j]] = [this.real[j], this.real[i]];
-                [this.imag[i], this.imag[j]] = [this.imag[j], this.imag[i]];
-            }
-        }
-
-        for (let i = 1; i < N; i <<= 1) {
-            costp = cos(PI / i);
-            sintp = sin(PI / i);
-
-            for (let j = 0; j < N; j += (i << 1)) {
-                real = 1;
-                imag = 0;
-
-                for (let k = 0; k < i; k++) {
-                    tpreal = real * this.real[j + k + i] - imag * this.imag[j + k + i];
-                    tpimag = real * this.imag[j + k + i] + imag * this.real[j + k + i];
-
-                    this.real[j + k + i] = this.real[j + k] - tpreal;
-                    this.imag[j + k + i] = this.imag[j + k] - tpimag;
-
-                    this.real[j + k] += tpreal;
-                    this.imag[j + k] += tpimag;
-
-                    tpreal = real * costp - imag * sintp;
-                    imag = real * sintp + imag * costp;
-                    real = tpreal;
-                }
-            }
-        }
-    }
-
-    get spectrum() {
-        const spectrum = new Array(this.size / 2);
-        for (let i = 0; i < this.size / 2; i++) {
-            spectrum[i] = Math.sqrt(this.real[i] ** 2 + this.imag[i] ** 2);
-        }
-        return spectrum;
-    }
-}
-
 const FFT_SIZE = 64;
 let eegBuffer = [];
 let audioElement = document.getElementById("neurofeedbackAudio");
@@ -153,9 +90,16 @@ window.processEEGData = function (uVrms) {
     console.log("eegBuffer length:", eegBuffer.length);
     if (eegBuffer.length >= FFT_SIZE) {
         console.log("Buffer lleno, calculando FFT...");
-        const fft = new FFT(FFT_SIZE, 256);
-        fft.forward(eegBuffer);
-        const frequencies = fft.spectrum;
+        const fft = new FFT(FFT_SIZE);
+        const phasors = fft.createComplexArray();
+        fft.realTransform(phasors, eegBuffer);
+        fft.completeSpectrum(phasors);
+
+        const frequencies = new Array(FFT_SIZE / 2);
+        for (let i = 0; i < FFT_SIZE / 2; i++) {
+            frequencies[i] = Math.sqrt(phasors[2 * i] ** 2 + phasors[2 * i + 1] ** 2);
+        }
+
         console.log("Frequencies calculated:", frequencies);
 
         // Asegurarse de que las frecuencias no estén vacías
